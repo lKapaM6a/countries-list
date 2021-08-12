f98a5e926f7264394c2dfb25b11845b797637f9c
Victor Berezin <victor.berezin@bws.pp.ua>
2021-08-12 15:17:35 +0300

    Updates

---
+++ precache-manifest.679d6149e94ba1b5a06171450e9c3e1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcab0426dca9f8ad1c2a4ebd9a08af2",
+    "url": "/countries-list/index.html"
+  },
+  {
+    "revision": "c471515853ace935702d",
+    "url": "/countries-list/static/css/main.9cf3c848.chunk.css"
+  },
+  {
+    "revision": "174436d0210c623d9907",
+    "url": "/countries-list/static/js/2.bf651803.chunk.js"
+  },
+  {
+    "revision": "c471515853ace935702d",
+    "url": "/countries-list/static/js/main.832eca6b.chunk.js"
+  },
+  {
+    "revision": "e59ddfba965c6bee0510",
+    "url": "/countries-list/static/js/runtime~main.6d3cfe5e.js"
+  }
+]);
